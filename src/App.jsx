bffc4d4928f19f87cdd299b2d37c6ca1cc841a5a
Brian Teeters <BrianTeetersDesign@gmail.com>
2025-10-22 18:13:32 -0400

    set up routing for home, shop, and cart pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { Route, Routes } from "react-router-dom"
+import Cart from "./pages/Cart/Cart"
+import Home from "./pages/Home/Home"
+import Shop from "./pages/Shop/Shop"
+
 function App() {
   return (
     <>
       <h1>Shop App Test</h1>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/shop" element={<Shop/>}/>
+        <Route path="/cart" element={<Cart/>}/>
+      </Routes>
     </>
   )
 }
